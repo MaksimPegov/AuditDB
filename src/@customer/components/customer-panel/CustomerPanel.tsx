47d2ab4dc5c67261778ff7e3bb6038828931bcf5
Anton Pegov <antonpegov@gmail.com>
2022-12-22 21:39:47 +0100

    [54] Multiple fixes

--- src/@customer/components/customer-panel/CustomerPanel.tsx
+++ src/@customer/components/customer-panel/CustomerPanel.tsx
@@ -59,10 +59,11 @@ export const CustomerPanel: React.FC<CustomerPanelProps> = ({
   const handleFieldChange = (
     event: React.ChangeEvent<HTMLInputElement>,
     field: string,
+    trim = true,
   ): void => {
     setCustomerData((prevState) => ({
       ...prevState,
-      [field]: event.target.value.trim(),
+      [field]: trim ? event.target.value.trim() : event.target.value,
     }))
 
     setErrors((prevState) => ({
@@ -190,7 +191,11 @@ export const CustomerPanel: React.FC<CustomerPanelProps> = ({
                 value={customerData.about}
                 error={errors.fname}
                 onChange={(e) =>
-                  handleFieldChange(e as React.ChangeEvent<HTMLInputElement>, 'about')
+                  handleFieldChange(
+                    e as React.ChangeEvent<HTMLInputElement>,
+                    'about',
+                    false,
+                  )
                 }
               />
             </Grid>
@@ -207,7 +212,11 @@ export const CustomerPanel: React.FC<CustomerPanelProps> = ({
                 value={customerData.company}
                 error={errors.fname}
                 onChange={(e) =>
-                  handleFieldChange(e as React.ChangeEvent<HTMLInputElement>, 'company')
+                  handleFieldChange(
+                    e as React.ChangeEvent<HTMLInputElement>,
+                    'company',
+                    false,
+                  )
                 }
               />
             </Grid>
